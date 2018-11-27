3b0d2553a8b88a8cc2209d8f21ee6ad812787e48
Jirka <del@hs-furtwangen.de>
2018-11-27 17:19:03 +0100

    Prepared for assignment

--- L06_SendData/Server/Server.js
+++ L06_SendData/Server/Server.js
@@ -2,14 +2,19 @@
 const Http = require("http");
 var L06_SendData;
 (function (L06_SendData) {
+    console.log("Starting server");
     let port = process.env.PORT;
     if (port == undefined)
         port = 8100;
     let server = Http.createServer();
     server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
     server.listen(port);
+    function handleListen() {
+        console.log("Listening");
+    }
     function handleRequest(_request, _response) {
-        console.log("Ich höre Stimmen!");
+        console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
